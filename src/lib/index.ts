52f7a22470237348d177a0655267f85149f93e59
rdietscht <rdietscht@gmail.com>
2025-07-21 00:20:40 -0400

    Adding new type `BreadStep_T` + created custom art

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,10 @@
 // place files you want to import through the `$lib` alias in this folder.
+type BreadStep_T =
+{
+    title: string, // the title of the step (i.e., making the dough)
+    img: string, // url to associated image
+    alt: string, // description of image (for accessibility purposes)
+    desc: string, // describes the step; can be expanded
+    links?: string, // resource links associated for additional help
+    timer?: number // for steps with waiting periods, the time in seconds to start the timer
+}
--- src/routes/progress/+page.svelte
+++ src/routes/progress/+page.svelte
@@ -1,3 +1,38 @@
-<main>
-    TODO
+<script lang="ts">
+
+    const steps: BreadStep_T[] = [
+        {
+            title: 'Make the dough',
+            img: '/doughmaking.png',
+            alt: 'cartoon of someone making dough',
+            desc: 'TODO: Fill these steps out',
+        }
+    ];
+
+</script>
+
+{#snippet breadstep(step: BreadStep_T)}
+
+    <!-- Render a window using the provided step details -->
+    <div class="border-2">
+        <img
+            class="border-2"
+            src={step.img}
+            alt={step.alt}
+        />
+    </div>
+
+{/snippet}
+
+<main class="w-full h-full rounded-md bg-amber-100 font-freckleface">
+
+    <!-- For sandwich bread, display the steps and have timers available -->
+    <h1 class="text-4xl text-center text-red-900 py-4">Sandwich Bread</h1>
+
+    <!-- Carousel for showing steps -->
+    <div>
+        <!-- PROOF OF CONCEPT; REMOVE WHEN NOT TESTING -->
+        {@render breadstep(steps[0])}
+    </div>
+
 </main>
