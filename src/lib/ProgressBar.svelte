bf5506412b480f9c8200cc43c6b9bc9bd6ac84c2
rdietscht <rdietscht@gmail.com>
2025-07-26 13:29:45 -0400

    Added functional navigation + ProgressBar skel.

---
+++ src/lib/ProgressBar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+    // The max number of steps to render + the current step highlighted.
+    let { stepCount, currentStep }: { stepCount: number, currentStep: number } = $props();
+
+    const MAX_COUNT: number = 8; // changes the maximum number of steps rendered
+    const STEP_VALUE: number = MAX_COUNT / stepCount; // the value of each step, used to progress bar when stepCount > MAX_COUNT
+
+</script>
+
+<!-- The vertical progress bar should render "levels"
+ of some container getting filled as each step is completed.
+ The render should only be less than one level filled when initialized.
+ When the currentStep is equal to the stepCount , the progress
+ bar container should appear full. In times where the stepCount
+ is larger than the maxCount constant, the progress bar will determine
+ the relative progress by standardizing each step of MAX_COUNT. -->
+
+<div class="w-full h-full border-x-2 border-b-2 border-stone-50 rounded-b-md">
+    
+</div>
--- src/routes/progress/+page.svelte
+++ src/routes/progress/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import ProgressBar from '$lib/ProgressBar.svelte';
 
+    // TODO: Move these into a static file that gets fetched before rendering the page.
     const steps: BreadStep_T[] = [
         {
             title: 'Make the Dough',
@@ -9,10 +11,74 @@
             requires careful kneading to provide a firm foundation \
             for your starter. If you want to ensure a good texture \
             for your bread, make sure you knead well!',
-        }
+        },
+        {
+            title: 'Stretch-N-Fold',
+            img: '/loading-dough.png',
+            alt: 'loading bread icon',
+            desc: 'I honestly have no clue wtf this means. I think\
+            you gotta do some yoga or sumn while making bread',
+        },
+        {
+            title: 'Bulk Ferment',
+            img: '/loading-dough.png',
+            alt: 'loading bread icon',
+            desc: 'Oh yeah it\'s that part that takes fuckin forever',
+        },
+        {
+            title: 'Shape Dough',
+            img: '/loading-dough.png',
+            alt: 'loading bread icon',
+            desc: 'It\'s gotta be like building a snowman right? But\
+            like instead of a friend you eat it when you\'re done and\
+            realize you wasted half your time of the week making food\
+            that is gonna be gone in less than 2 days',
+        },
+        {
+            title: 'Second Rise',
+            img: '/loading-dough.png',
+            alt: 'loading bread icon',
+            desc: 'U gotta be fkn kidding me. ANOTHER WAIT?? Dude just\
+            go buy some bread at Wal-Mart or sumn this shit is taking \
+            forever and I\'m hungry',
+        },
+        {
+            title: 'Preheat Oven (400 degs.)',
+            img: '/loading-dough.png',
+            alt: 'loading bread icon',
+            desc: 'Time to burn that MF like the Terminator. I think this also\
+            requires some timing bc you want the inside to be the right texture\
+            so make sure to ask Gia/Mami how they typically do this part.',
+        },
+        {
+            title: 'Bake Bread (covered)',
+            img: '/loading-dough.png',
+            alt: 'loading bread icon',
+            desc: 'jesus this thing is complicated. how the hell did humanity\
+            even discover this shit without the wonders of technology? like\
+            how would you even realize you need to bake covered then uncovered...',
+        },
+        {
+            title: 'Bake Bread (uncovered)',
+            img: '/loading-dough.png',
+            alt: 'loading bread icon',
+            desc: '.. you dirty dog you...',
+        },
     ];
 
+    // STATE
     let showGUI: boolean = $state(true);
+    let currentStep: number = $state(0);
+
+    // HELPERS
+    function stepBack()
+    {
+        currentStep = currentStep - 1 < 0 ? 0 : currentStep - 1;
+    }
+    function stepForward()
+    {
+        currentStep = currentStep + 1 >= steps.length ? steps.length - 1 : currentStep + 1;
+    }
 
 </script>
 
@@ -46,8 +112,7 @@
     <div class="h-2/3">
         <h2 class="h-1/9 text-2xl text-center">Current Step:</h2>
         <div class="w-full h-8/9">
-            <!-- PROOF OF CONCEPT; REMOVE WHEN NOT TESTING -->
-            {@render breadstep(steps[0])}
+            {@render breadstep(steps[currentStep])}
         </div>
     </div>
 
@@ -56,22 +121,33 @@
         <button
             class="bg-red-900 text-stone-50 px-2 py-1 rounded-tr-sm"
             onclick={() => { showGUI = !showGUI }}
-        >Show Hidden</button>
-        <div class={`${ showGUI ? 'max-h-100' : 'max-h-0'} h-fit overflow-clip transition-[max-height] bg-red-900 text-stone-50`}>
-    
+        >Show Navigation</button>
+        <div class={`${ showGUI ? 'max-h-100' : 'max-h-0'} h-fit overflow-clip transition-[max-height] bg-red-900 text-stone-50 flex flex-row`}>
+
             <!-- Navigational Buttons + Progress Bar (steps) -->
-            <div>
-                <h3>Progress</h3>
+            <div class="w-1/2 border-2">
+                <h3 class="text-center text-lg">Progress</h3>
                 <div class="flex flex-row">
-                    <button class="">Prev.</button>
-                    <button class="">Next</button>
+                    <div class="w-3/5 flex flex-row justify-evenly items-center">
+                        <button
+                            onclick={stepBack}
+                            class="w-1/3 h-fit bg-purple-500 shadow-lg transition active:shadow-purple-500/50 active:scale-110"
+                        >Prev.</button>
+                        <button
+                            onclick={stepForward}
+                            class="w-1/3 h-fit bg-purple-500 shadow-lg transition active:shadow-purple-500/50 active:scale-110"
+                        >Next</button>
+                    </div>
+                    <div class="w-2/5 h-20 px-2 py-1">
+                        <ProgressBar stepCount={8} currentStep={1} />
+                    </div>
                 </div>
             </div>
 
             <!-- Timers (if any) + "BreadBook" containing facts about the selected bread -->
-            <div class="flex flex-col">
+            <div class="flex flex-col w-1/2">
                 <div>
-                    <h3>Timers</h3>
+                    <h3 class="text-lg">Timers</h3>
                 </div>
                 <div>
                     <!-- TODO: Make this into cartoon of a book -->
