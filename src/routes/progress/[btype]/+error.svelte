ad9533080178fc855fb821f98f6a0a22ed5ea1ed
rdietscht <rdietscht@gmail.com>
2025-08-16 10:46:54 -0400

    Added image to an error page

--- src/routes/progress/[btype]/+error.svelte
+++ src/routes/progress/[btype]/+error.svelte
@@ -5,4 +5,10 @@
 
 <div class="m-auto w-7/8 h-full px-2 py-1 bg-orange-200">
     <h1>Oops! Something we couldn't find that bread type.</h1>
+
+    <img
+        class="rounded-md"
+        src="/error-bread.png"
+        alt="person watching burnt bread"
+    />
 </div>
